a4c85fc73b7b70f115985268baae0026b2fc641e
Merk68 <marko.mark2001@gmail.com>
2021-12-16 11:02:34 +0100

    Add project files.

---
+++ Vasmegye/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Vasmegye
+{
+  class Program
+  {
+    static List<szamok> lista = new List<szamok>();
+    static void Main(string[] args)
+    {
+      Beolvasas();
+      HarmadikFeladat();
+      Console.WriteLine($"{lista.Count}");
+      Console.ReadLine();
+    }
+
+    private static void HarmadikFeladat()
+    {
+      
+    }
+
+    private static void Beolvasas()
+    {
+      StreamReader be = new StreamReader("vas.txt");
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split('-');
+        lista.Add(new szamok(Convert.ToInt32(a[0]),Convert.ToInt32(a[1]), Convert.ToInt32(a[2])));
+      }
+      be.Close();
+    }
+  }
+}
---
+++ Vasmegye/szamok.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vasmegye
+{
+  class szamok
+  {
+    private int szulesnem;
+
+    public int Szulesnem
+    {
+      get { return szulesnem; }
+      set { szulesnem = value; }
+    }
+    private int szamok2;
+
+    public int Szamok2
+    {
+      get { return szamok2; }
+      set { szamok2 = value; }
+    }
+    private int szamok3;
+
+    public int Szamok3
+    {
+      get { return szamok3; }
+      set { szamok3 = value; }
+    }
+
+    public szamok(int szulesnem, int szamok2, int szamok3)
+    {
+      this.szulesnem = szulesnem;
+      this.szamok2 = szamok2;
+      this.szamok3 = szamok3;
+    }
+  }
+}
